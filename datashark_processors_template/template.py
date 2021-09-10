456a4c229298a44d3a49b7ca35bc776cb0d4ad21
koromodako <koromodako@gmail.com>
2021-09-10 23:55:48 +0200

    Update template

--- datashark_processors_template/template.py
+++ datashark_processors_template/template.py
@@ -1,11 +1,10 @@
 """Datashark Template Plugin
 """
-from typing import List, Tuple, Optional
-from pathlib import Path
+from typing import List
 from datashark_core.meta import ProcessorMeta
 from datashark_core.logging import LOGGING_MANAGER
-from datashark_core.processor import ProcessorInterface
-from datashark_core.model.api import System, ProcessorArgument
+from datashark_core.processor import ProcessorInterface, ProcessorError
+from datashark_core.model.api import Kind, System, ProcessorArgument
 
 NAME = 'template'
 LOGGER = LOGGING_MANAGER.get_logger(NAME)
@@ -16,26 +15,28 @@ class TemplateProcessor(ProcessorInterface, metaclass=ProcessorMeta):
 
     NAME = NAME
     SYSTEM = System.INDEPENDENT
-    ARGUMENTS = []
+    ARGUMENTS = [
+        {
+            'name': 'template_arg',
+            'kind': Kind.STR,
+            'value': 'default_val',
+            'required': False,
+            'description': """
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                qui officia deserunt mollit anim id est laborum
+            """
+        },
+    ]
     DESCRIPTION = """
     Template of a processor, not meant for use, meant for dev
     """
 
-    async def _run(
-        self, filepath: Path, arguments: List[ProcessorArgument]
-    ) -> Tuple[bool, Optional[str]]:
+    async def _run(self, arguments: List[ProcessorArgument]):
         """Process a file using tskape"""
-        status = False
-        details = None
-        try:
-            # TODO: perform artifact processing here
-            raise NotImplementedError()
-            # commit data added by plugin (if needed)
-            #self.session.commit()
-            # finally set overall processing status to SUCCESS
-            status = True
-        except:
-            LOGGER.exception(
-                "an exception occured while processing filepath: %s", filepath
-            )
-        return status, details
+        # TODO: perform processor work here
+        raise ProcessorError("not implemented!")
+        # commit data added by plugin (if needed)
+        #self.session.commit()
